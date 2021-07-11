3becbc5b25f0f863181b38ef6c1f5ab9147a8525
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-07-11 15:17:29 -0700

    appended svg and svg group

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,24 @@
-// @TODO: YOUR CODE HERE!
+// Determine SVG wrapper dimensions
+
+var svgWidth = 1200;
+var svgHeight = 660;
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.right - margin.left;
+
+// Create SVG Wrapper
+var svg = d3.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+// Add an SVG group
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
